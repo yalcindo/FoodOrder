3b7a44d5b0d281cd422ea328125d301f8c4162a1
yalcindo <yalcindo1@gmail.com>
2013-10-11 11:21:48 -0600

    Final changes

--- js/index.js
+++ js/index.js
@@ -11,14 +11,15 @@ var FoodItem = function(name,calories,vegan,glutenFree,citrusFree){
  
 };
     // Instantiating each food item
-	var chicken = new FoodItem("Chicken",100,false,false,false);
+	var chicken = new FoodItem("Chicken",100,false,true,true);
 	var bean = new FoodItem("Bean",940,true,false,true);
 	var egg = new FoodItem("Egg",160,true,true,false);
 	var cilantro= new FoodItem("Cilantro",5,true,true,true);
 	var guacamole= new FoodItem("Guacamole",5,true,true,true);
 	var lettuce= new FoodItem("Lettuce",5,true,true,true);
 	var tequila= new FoodItem("Tequila",65,true,true,true);
-	var lemon= new FoodItem("LeMon",5,true,true,true);
+	var lemon= new FoodItem("LeMon",5,true,true,false);
+	var meat =new FoodItem("Meat",968,false,true,true);
 
 FoodItem.prototype.toString = function(){return this.name.toString()};
 //creates Drink constructor
@@ -84,7 +85,7 @@ Plate.prototype.toString = function(){return  this.name +"\n"+"Ingredents:"+this
 	"isGlutenFree:"+this.isGlutenFree()+"\n"+"Price :"+this.price};
 
 Drink.prototype.toString = function(){return this.name+"\n" 
-	+ "Description: "+this.description+"\n" +"Ingredients: "+this.items+"\n"+"Vegan:"+Plate.prototype.isVegan.call(this)+"\n"+"CitrusFree:"+
+ 	+ "Description: "+this.description+"\n" +"Ingredients: "+this.items+"\n"+"Vegan:"+Plate.prototype.isVegan.call(this)+"\n"+"CitrusFree:"+
 	Plate.prototype.isCitrusFree.call(this)+"\n"+"GlutenFree:"+Plate.prototype.isGlutenFree.call(this)+"\n"+"Price:"+this.price+"\n"};
 
 
@@ -123,14 +124,14 @@ var Customer = function(dietaryPreference){
 Customer.prototype.toString = function(){return " dietaryPreference: "+ this.dietaryPreference};
 
 //Instantiate a burrito plate
-var Burrito = new Plate("Burrito","A real Tex-mex dish",7.89,[bean,bean,cilantro,guacamole,chicken,lettuce]); 
+var Burrito = new Plate("Burrito","A real Tex-mex dish",7.89,[bean,chicken,cilantro,guacamole,chicken,lettuce]); 
 
 //Instantiate a Guacamole plate
 var Guacamole = new Plate("Guacamole","Made with fresh Avacado",2.5,[lemon,lettuce,egg]); 
 //Instantiate a Margarita Dring
-var Margarita = new Drink("Margarita","Made with love for lovers","Tequila,and other sweet stuff");
-var menu1 = new Menu([Burrito,Guacamole,Margarita]);
- var menuprint=menu1.toString();
-console.log(menuprint);
+var Margarita = new Drink("Margarita","Made with love for lovers",[lemon,tequila]);
+// var menu1 = new Menu([Burrito,Guacamole,Margarita]);
+//  var menuprint=menu1.toString();
+// console.log(menuprint);
 
 
--- js/pageLoad.js
+++ js/pageLoad.js
@@ -1,60 +1,87 @@
 $(document).ready( function(){
 	
-   var numbers={
-   	total:0
-   };
+  var numbers={
+    total:0
+  };
 
 
-	var Burrito = new Plate("Burrito","A real Tex-mex dish",7.89,[bean,bean,cilantro,guacamole,lettuce,lettuce]); 
+	var Burrito1 = new Plate("Burrito","A real Tex-mex dish Vegan",7.89,[bean,lemon,cilantro,guacamole,lettuce,lettuce]); 
+  var Burrito2 = new Plate("Burrito","A real Tex-mex dish with chicken",7.89,[bean,chicken,cilantro,guacamole,lettuce,lettuce]); 
+  var Burrito3 = new Plate("Burrito","A real Tex-mex dish with meat",7.89,[bean,meat,cilantro,guacamole,lettuce,lettuce]); 
 	var Guacamole = new Plate("Guacamole","Made with fresh Avacado",2.5,[guacamole,lemon,lettuce,egg]); 
-	var Margarita = new Drink("Margarita","Made with love for lovers",[tequila,lemon],4.5);
+	var Margarita = new Plate("Margarita","Made with love for lovers",4.5,[tequila,lemon]);
 
-	 var createPlateArray= function(plate){
+	var createPlateArray= function(plate)
+  {
 	 	return plate.toString().split("\n");
- };
+  };
     
-	Plate.prototype.create = function(appendPlate){
-		
-		var added= $('<div class="food-item" data-price='+appendPlate.price+' data-vegan='+appendPlate.isVegan()+'><span class="food-name">{0}</span><br> {1}<br>  {2} <br> <span class="vegan-food">{3}</span><br>  {4}<br>  {5}<br> {6}</div>'.supplant(createPlateArray(appendPlate)));
-		 
+	Plate.prototype.create = function(appendPlate)
+  {
+		var added= $('<div class="food-item" data-price='+appendPlate.price+ ' data-vegan='+ appendPlate.isVegan() + ' data-citrus='+ appendPlate.isCitrusFree() + ' data-gluten='+ appendPlate.isGlutenFree()+ '><span class="food-name">{0}</span><br> {1}<br>  {2} <br> <span class="vegan-food">{3}</span><br>  {4}<br>  {5}<br> {6}</div>'.supplant(createPlateArray(appendPlate)));
 		return $(".menu").append(added);
 	};
 
     
-   $(".menu").on("click",".food-item",function(){
-     var itemPrice=$(this).data("price");
+   $(".menu").on("click",".food-item",function()
+    {
+      var itemPrice=$(this).data("price");
       numbers.total+=itemPrice;
       $(".total").empty();
-       var orderPrice=$('<div class="total"> Total price : '+numbers.total+'</div>');
-       var orderItem=$(this).find(".food-name").text();
-       var orderItemAppend=$('<div class="order-item"> '+orderItem+" "+itemPrice+'</div>');
-       $(".order").append(orderItemAppend);
-       $(".order").append(orderPrice);
-   });
+      var orderPrice=$('<div class="total"> Total price : '+numbers.total+'</div>');
+      var orderItem=$(this).find(".food-name").text();
+      var orderItemAppend=$('<div class="order-item"> '+orderItem+" "+itemPrice+'</div>');
+      $(".order").append(orderItemAppend);
+      $(".order").append(orderPrice);
+    });
 
 
-   var getButtonValue=function(){
-        var value=$(".food-item").data("vegan");
-        console.log("value"+value)
-        
+   var getVeganValue=function(){
+             $(".food-item").show();
+        $(".food-item").each(function(){
+           var value=$(this).data("vegan");
+           //console.log("value:"+value);
+           value ? true :$(this).hide();
+           
+        });
+    };
+    var getCitrusValue=function(){
+           $(".food-item").show();
+          $(".food-item").each(function(){
+           var value=$(this).data("citrus");
+           // console.log("value:"+value);
+           value ? true :$(this).hide();
+           
+        });
+    };
+     var getGlutenValue=function(){
+           $(".food-item").show();
+           $(".food-item").each(function(){
+           var value=$(this).data("gluten");
+            console.log("value:"+value);
+           value ? true :$(this).hide();
+           
+        });
+    };
+     
 
-   };
 
-  $(".costumer-info").on("click","button",getButtonValue);
-
-
-
+  $(".costumer-info").on("click",".button-vegan",getVeganValue);
+  $(".costumer-info").on("click",".button-citrus",getCitrusValue);
+  $(".costumer-info").on("click",".button-gluten",getGlutenValue);
 
 
 
 
 
-Plate.prototype.create(Burrito);
+
+
+Plate.prototype.create(Burrito1);
 Plate.prototype.create(Guacamole);
-// Plate.prototype.create(Margarita);
-// Plate.prototype.create(Burrito);
-// Plate.prototype.create(Margarita);
-// Plate.prototype.create(Burrito);
-// Plate.prototype.create(Margarita);
+Plate.prototype.create(Margarita);
+Plate.prototype.create(Burrito2);
+Plate.prototype.create(Margarita);
+Plate.prototype.create(Burrito3);
+Plate.prototype.create(Margarita);
 
 });
